984a2bca39a1cfb4a53b5f9ccb3fa933191fa8f4
DanteFrank <davidfrankoziwo@gmail.com>
2022-07-27 11:14:16 +0100

    Self-sgined cert setup

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
@@ -13,6 +15,14 @@ app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+https.createServer({
+      'cert': fs.readFileSync('cert.pem'),
+      'key': fs.readFileSync('key.pem'),
+   }, app).listen(PORT, () => {
    console.log(`Server running and listening on ${PORT}`);
 });
+
+/*  
+How to geneartae a self signed certificate:
+"openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365"
+*/
